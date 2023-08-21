2b7fe27803670b9c7e01106c2e9fddadc52710c2
Andres Mora <andres.mora@dracobots.com>
2023-08-21 21:25:33 +0100

    add mock.ts

--- __tests__/application/useCases/getCliUseCaseTest.ts
+++ __tests__/application/useCases/getCliUseCaseTest.ts
@@ -1,5 +1,18 @@
+import GetCliUseCase from "@/application/useCases/test/GetCliUseCase";
+import ITestAction from "@/domain/interfaces/infrastructure/persistence/redux/iTestAction";
+import IApiTest from "@/domain/interfaces/infrastructure/service/api/iApiTest";
 import { describe, test } from "@jest/globals";
+import { Mock } from "moq.ts";
 
 describe("Get exchange information", () => {
-  test("happy path get information", async () => {});
+  test("happy path get information", async () => {
+    //ITestAction
+    //IApiTest
+    const action = new Mock<ITestAction>();
+    const api = new Mock<IApiTest>();
+    const application = new GetCliUseCase(action.object(), api.object());
+
+    let a = true;
+    expect(a).toBe(true);
+  });
 });
